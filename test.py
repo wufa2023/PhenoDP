d7a3cd13f50efa8b2398167a0c852fa8dedcec82
wufa2023 <blwen2020nk@gmail.com>
2024-03-09 20:44:35 +0800

    for hpo number list

--- test.py
+++ test.py
@@ -1,16 +1,5 @@
-import requests
-import pandas as pd
+# 第一步：预处理ATAC maxtrix
+mt1 = read(mt1_path)
+mt2 = read(mt2_path)
 
-url = "https://hpo.jax.org/api/hpo/term/HP:0000002/diseases"
-response = requests.get(url)
-print(1)
-if response.status_code == 200:
-    data = response.json()
-    df = pd.DataFrame(data['diseases'])
-
-    # Filter rows where 'db' column is 'OMIM'
-    omim_df = df.loc[df['db'] == 'OMIM']
-
-    print(omim_df)
-else:
-    print("Failed to retrieve data. Status code:", response.status_code)
+# 组内筛选差异
