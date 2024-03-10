ddaacd28094f2087920fa4a256fc0bd5a0cd1994
wufa2023 <blwen2020nk@gmail.com>
2024-03-10 10:53:00 +0800

    for hpo number list

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
-# 第一步：预处理ATAC maxtrix
-mt1 = read(mt1_path)
-mt2 = read(mt2_path)
+from collections import defaultdict
 
-# 组内筛选差异
+# 读取包含 HPO 和 OMIM 关系的文本文件
+with open("source_data/hpo_to_omim_results.txt", "r") as file:
+    lines = file.readlines()
+
+# 创建一个字典来存储每个 OMIM 号对应的 HPO 号列表
+omim_to_hpo = defaultdict(list)
+
+# 遍历每一行，将 HPO 和 OMIM 关系存储到字典中
+for line in lines:
+    hpo, omim = line.strip().split(",")
+    omim_to_hpo[omim].append(hpo)
+
+# 打印每个 OMIM 号对应的 HPO 号列表
+for omim, hpos in omim_to_hpo.items():
+    print(f"{omim} -> {', '.join(hpos)}")
