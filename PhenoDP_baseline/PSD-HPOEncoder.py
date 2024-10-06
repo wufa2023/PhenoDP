013ae0439d23f96d1c9eaf513d73bd2c82491c9b
wufa2023 <150779173+wufa2023@users.noreply.github.com>
2024-10-06 21:49:58 +0800

    Update PSD-HPOEncoder.py

--- PhenoDP_baseline/PSD-HPOEncoder.py
+++ PhenoDP_baseline/PSD-HPOEncoder.py
@@ -1,16 +1,69 @@
+from sentence_transformers import SentenceTransformer, LoggingHandler
+from sentence_transformers import models, util, datasets, evaluation, losses
+from transformers import AutoTokenizer, T5ForConditionalGeneration
+from torch.utils.data import DataLoader
 import torch
-import torch.nn as nn
+import pandas as pd
+import numpy as np
+from pyhpo.ontology import Ontology
+import re
+from PhenoDP_Preprocess import PhenoDP_Initial
+from pyhpo.ontology import Ontology
+import random
 import torch.nn.functional as F
+import pickle
+from scipy.stats import fisher_exact
 
+def compute_kernel_bias(vecs, n_components):
+    vecs = np.concatenate(vecs, axis=0)
+    mu = vecs.mean(axis=0, keepdims=True)
+    cov = np.cov(vecs.T)
+    u, s, vh = np.linalg.svd(cov)
+    W = np.dot(u, np.diag(s**0.5))
+    W = np.linalg.inv(W.T)
+    W = W[:, :n_components]
+    return W, -mu
+
+def transform_and_normalize(vecs, kernel, bias):
+    if not (kernel is None or bias is None):
+        vecs = (vecs + bias).dot(kernel)
+    return vecs / (vecs**2).sum(axis=1, keepdims=True)**0.5
+
+def normalize(vecs):
+    return vecs / (vecs**2).sum(axis=1, keepdims=True)**0.5
+
+def get_vec_for_d(d): 
+    vec = 0
+    for hp in d.hpo:
+        vec += node_em[Ontology.get_hpo_object(hp).id]
+    vec = vec / len(d.hpo)    
+    return vec
+
+def get_average_embedding(text):
+    input_ids = tokenizer.encode(text, return_tensors='pt')
+    with torch.no_grad():
+        outputs = T5model.encoder(input_ids=input_ids)
+        embeddings = outputs.last_hidden_state
+    embeddings = embeddings[0].numpy()
+    average_embedding = np.mean(embeddings, axis=0)
+    return average_embedding
+    
+def process_sentence(sentence):
+    tokens = tokenizer.tokenize(sentence)
+    filtered_tokens = [token for token in tokens if token not in high_freq_tokens]
+    token_ids = tokenizer.convert_tokens_to_ids(filtered_tokens)
+    return token_ids
+    
+def get_vec(hp):
+    obj = Ontology.get_hpo_object(hp)
+    return get_average_embedding(obj.name)
+    
 def nx_to_dgl(nx_graph):
     dgl_graph = dgl.from_networkx(nx_graph)
     dgl_graph = dgl.add_self_loop(dgl_graph)
     features = [nx_graph.nodes[node]['feature'] for node in nx_graph.nodes()]
     dgl_graph.ndata['feat'] = torch.tensor(features, dtype=torch.float32)
     return dgl_graph
-
-dgl_graph = nx_to_dgl(graph)
-feature_dimension = 768
 
 def mask_graph(dgl_graph, node_mask_percentage=0.2, edge_mask_percentage=0.2):
     num_nodes = dgl_graph.num_nodes()
@@ -58,8 +111,3 @@ def train_model(model, graph, epochs=2000, lr=0.001, node_mask_percentage=0.2, e
         
         if epoch % 10 == 0:
             print(f'Epoch {epoch}, Loss: {loss.item()}')
-
-train_model(model, dgl_graph)
-model.eval()
-with torch.no_grad():
-    reconstructed_features, reconstructed_latent = model(dgl_graph, dgl_graph.ndata['feat'])
